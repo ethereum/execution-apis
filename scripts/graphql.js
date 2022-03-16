bddfda4523de361fe72364bb32478e612873b355
Sina Mahmoodi <itz.s1na@gmail.com>
2022-03-16 16:20:37 +0100

    Add graphql validation

--- scripts/graphql.js
+++ scripts/graphql.js
@@ -1,14 +1,15 @@
 import fs from 'fs';
-import { buildClientSchema, printSchema, getIntrospectionQuery } from 'graphql';
+import graphql from 'graphql';
 import { request } from 'graphql-request';
 
 const endpoint = 'http://localhost:8545/graphql';
 
 async function main() {
-    const q = getIntrospectionQuery();
+    const q = graphql.getIntrospectionQuery();
     const res = await request(endpoint, q);
     const schema = JSON.stringify(res, null, 2);
     fs.writeFileSync('graphql.json', schema);
+    console.log('GraphQL schema generated successfully.')
 }
 
-main().then().catch((err) => console.log(err))
+main().then().catch((err) => console.log(err));
