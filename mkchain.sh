43d198051753c034661852c3f9fac6ff2fa6b6ce
Felix Lange <fjl@twurst.com>
2025-02-11 12:13:36 +0100

    chain: recreate chain with non-zero fork spacing

--- mkchain.sh
+++ mkchain.sh
@@ -1,9 +1,9 @@
 mkdir -p chain/
 ./hivechain generate \
     -outdir chain/       \
-    -length 20           \
+    -length 45           \
     -tx-count 4          \
     -tx-interval 1       \
-    -fork-interval 0     \
+    -fork-interval 3     \
     -lastfork prague     \
     -outputs genesis,chain,forkenv,headstate,txinfo,accounts,headfcu
