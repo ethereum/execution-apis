e4a3ddda399e60cb5acdbab56d54b39725d67d35
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2025-05-14 16:49:40 -0400

    update baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://acolytec3.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/execution-apis/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
