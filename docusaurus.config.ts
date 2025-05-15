87e206e4be3b96bd021dac059efa180704b0dd67
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2025-05-15 10:38:09 -0400

    update links to point to ethereum

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,14 +10,14 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://acolytec3.github.io',
+  url: 'https://ethereum.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/execution-apis/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'acolytec3', // Usually your GitHub org/user name.
+  organizationName: 'ethereum', // Usually your GitHub org/user name.
   projectName: 'execution-apis', // Usually your repo name.
   deploymentBranch: 'gh-pages',
 
